2e25feb9790416aec02a24ea93cee5e6a33ad918
Matthew Beckwith <kodahScripts@gmail.com>
2017-07-01 21:58:28 -0400

    official final product, some css tweaks and a special message in the console for my viewers.

---
+++ js/consoleMsg.js
@@ -0,0 +1,13 @@
+if( typeof console === 'object' ) {
+    console.log(
+        '\n' +
+        'Hi there, fellow developer! Thanks for visiting.     ("`-’-/").___..--’’"`-._         \n' +
+        '                                                      `6_ 6  )   `-.  (     ).`-.__.‘)\n' +
+        'Email: kodahscripts@outlook.com                       (_Y_.)’  ._   )  `._ `. ``-..-’ \n' +
+        '                                                    _..`--’_..-_/  /--’_.’ ,’         \n' +
+        'I’d love to hear what you think!                   (il),-’‘  (li),’  ((!.-‘           \n' +
+        '\n' +
+        '                                 Matthew Beckwith\n' +
+        '                                 — @kodahScripts\n'
+    );
+}
