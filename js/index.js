b226a042b51199d3ccf6bdea8d61760a6c5d62ed
Cody Barber <cody.n.barber@gmail.com>
2018-02-13 12:40:14 -0500

    ENH: Logic added for JS email validation

--- js/index.js
+++ js/index.js
@@ -2,28 +2,53 @@ var app = new Vue({
   el: '#app',
   data: {
     name: '',
-    name_error: false,
+    invalid_name: null,
     email: '',
-    email_error: false,
+    invalid_email: null,
     phone: '',
-    invalid_phone: false,
+    invalid_phone: null,
     validation: {
+      name: {
+        not_empty: {
+          valid: false,
+          message: 'Name cannot be empty'
+        }
+      },
       email: {
-        not_empty: false,
-        letters: false,
-        atmark: false,
-        domain: false,
-        extension: false,
-        domain_length: false,
-        extension_length: false
+        not_empty: {
+          valid: false,
+          message: 'Cannot be empty'
+        },
+        passed: {
+          valid: false,
+          message: 'Email address is not valid'
+        }
       },
       phone: {
-        not_empty: false,
-        valid_number: false,
-        digit_minimum: false,
-        digit_below_max: false,
-        non_repeating_digits: false,
-        pattern_check_clear: false
+        not_empty: {
+          valid: false,
+          message: 'Cannot be empty'
+        },
+        valid_number: {
+          valid: false,
+          message: 'Must be a valid number'
+        },
+        digit_minimum: {
+          valid: false,
+          message: 'Must have at least 10 digits'
+        },
+        digit_below_max: {
+          valid: false,
+          message: 'Cannot have more than 15 digits'
+        },
+        non_repeating_digits: {
+          valid: false,
+          message: 'Cannot begin with three repeating digits'
+        },
+        pattern_check_clear: {
+          valid: false,
+          message: 'Cannot contain prohibited pattern'
+        },
       }
     }
   },
@@ -31,8 +56,26 @@ var app = new Vue({
     trim: function(string) {
       return string.replace(/^\s+|\s+$/, '');
     },
+    verify_name: function(name) {
+      if (name != '') {
+        this.validation.name.not_empty = true;
+      } else {
+        this.validation.name.not_empty = false;
+      }
+    },
     verify_email: function(email) {
+      let trimmed = this.trim(email);
 
+      if (email != '') {
+        this.validation.email.not_empty = true;
+      } else {
+        this.validation.email.not_empty = false;
+      }
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(trimmed)) {
+        this.validation.email.passed = true;
+      } else {
+        this.validation.email.passed = false; 
+      }
     },
     verify_phone: function(phone) {
       const prohibited_numbers = [
@@ -83,14 +126,47 @@ var app = new Vue({
       }
     }
   },
+  computed: {
+    invalid_form: function() {
+      if (this.invalid_name == true || this.invalid_email == true || this.invalid_phone == true) {
+        return true;
+      } else if (this.invalid_name == null || this.invalid_email == null || this.invalid_phone == null){
+        return true;
+      }
+    }
+  },
   watch: {
+    name: _.debounce(function() {
+      this.edited = true;
+      let self = this;
+      self.invalid_name = false;
+      self.verify_name(this.name);
+      _.forIn(this.validation.name, function(v, k) {
+        if (v === false) {
+          self.invalid_name = true;
+        }
+      })
+    }, 500),
+    email: _.debounce(function() {
+      this.edited = true;
+      let self = this;
+      self.invalid_email = false;
+      self.verify_email(this.email);
+      _.forIn(this.validation.email, function(v, k) {
+        if (v === false) {
+          self.invalid_email = true;
+        }
+      })
+    }, 500),
     phone: _.debounce(function() {
+      this.edited = true;
       let self = this;
       self.invalid_phone = false;
       self.verify_phone(this.phone);
       _.forIn(this.validation.phone, function(v, k) {
         if (v === false) {
           self.invalid_phone = true;
+          return;
         }
       })
     }, 500)
